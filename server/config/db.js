9a7bc0afcd92bae0829e3b79b9dd85f0a55262d7
AyuniJati <ayunijati002@gmail.com>
2026-05-28 01:11:19 +0700

    data?

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,61 @@
 const mysql = require('mysql2')
 
+// koneksi awal tanpa database
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
-  database: 'kelulusan'
+  password: '1234'
 })
 
 db.connect((err) => {
   if (err) {
     console.log('Database gagal terhubung')
     console.log(err)
-  } else {
-    console.log('Database berhasil terhubung')
+    return
   }
+
+  console.log('MySQL berhasil terhubung')
+
+ // database
+  db.query(
+    `CREATE DATABASE IF NOT EXISTS kelulusan`,
+    (err) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+
+      console.log('Database kelulusan siap')
+
+      // gunakan database
+      db.query(`USE kelulusan`, (err) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+
+        // buat tabel siswa
+        const siswaTable = `
+          CREATE TABLE IF NOT EXISTS siswa (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nisn VARCHAR(20) UNIQUE,
+            nama VARCHAR(150),
+            status VARCHAR(20),
+            password VARCHAR(100)
+          )
+        `
+
+        db.query(siswaTable, (err) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+
+          console.log('Tabel siswa siap')
+        })
+      })
+    }
+  )
 })
 
 module.exports = db
